02fc43b0fbd203925b88742f97ef91a9a630a86e
xiaokai.zhou@westwell-lab.com <xiaokai.zhou@westwell-lab.com>
2020-06-12 03:24:37 +0800

    Delete dependencies of pangolin

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@
 #include "util/settings.h"
 #include "FullSystem/FullSystem.h"
 #include "util/Undistort.h"
-#include "IOWrapper/Pangolin/PangolinDSOViewer.h"
+// #include "IOWrapper/Pangolin/PangolinDSOViewer.h"
 #include "IOWrapper/OutputWrapper/SampleOutputWrapper.h"
 
 
@@ -215,10 +215,10 @@ int main( int argc, char** argv )
     fullSystem->linearizeOperation=false;
 
 
-    if(!disableAllDisplay)
-	    fullSystem->outputWrapper.push_back(new IOWrap::PangolinDSOViewer(
-	    		 (int)undistorter->getSize()[0],
-	    		 (int)undistorter->getSize()[1]));
+    // if(!disableAllDisplay)
+	  //   fullSystem->outputWrapper.push_back(new IOWrap::PangolinDSOViewer(
+	  //   		 (int)undistorter->getSize()[0],
+	  //   		 (int)undistorter->getSize()[1]));
 
 
     if(useSampleOutput)
